a287875d7eea5b9bd2722f2549ea168676147cf3
MennaLana <engmenna29@gmail.com>
2023-01-30 13:37:06 +0400

    first commit

---
+++ popup.js
@@ -0,0 +1,18 @@
+var modal = document.getElementsByClassName("popup")[0];
+
+// Get the button that opens the modal
+var btn = document.getElementsByClassName("trigger_popup_fricc")[0];
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
